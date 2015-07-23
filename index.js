bd24297b08c473977eae8a54fd22ec1b8106edaa
Max Mueller <maxmueller@eaton.com>
2015-07-23 10:19:29 -0400

    Bugfix for windows press (updated)
    - Accidentally removed check for 'return', rather than for 'enter'.
    Check is now "key.name === 'return'"
    - Previous commit had added a .trim() to the boolean type in multi
    because 'return' adds a newline character, now removed from boolean and
    added to cb in enter keypress (line.trim).

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ function prompt (message, hideInput, cb) {
       if (key.ctrl && key.name === 'c') {
         process.exit();
       }
-      else if (key.name === 'enter') {
+      else if (key.name === 'return') {
         process.stdin.removeListener('keypress', listen);
         process.stdin.pause();
         if (hideInput) {
           setRawMode(false);
           console.log();
         }
-        cb(line, function () {}); // for backwards-compatibility, fake end() callback
+        cb(line.trim(), function () {}); // for backwards-compatibility, fake end() callback
         return;
       }
       if (key.name === 'backspace') line = line.slice(0, -1);
@@ -97,8 +97,7 @@ function multi (questions, cb) {
     }
     else if (q.type === 'boolean') {
       label += '(y/n) ';
-      q.validate = function (val) {
-        val = val.trim();
+      q.validate = function (val){
         if (!val.match(/^(yes|ok|true|y|no|false|n)$/i)) return false;
       };
     }
